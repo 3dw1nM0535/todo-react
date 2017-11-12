1c26a864c2b9f78fe3ba128982bad98c3ab87584
3dw1nM0535 <workockmoses@gmail.com>
2017-11-12 18:11:38 +0300

    implemented delete functionality

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,49 @@
-alert('It works!');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import TodoItem from '../components/TodoItem';
+
+class Todo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ['Wash utensils', 'Prepare supper', 'Time for dinner', 'Practice coding'],
+      isToggleOn: true
+    };
+
+    this.onDelete = this.onDelete.bind(this);
+
+  }
+
+  onDelete (item) {
+    var updatedTodos = this.state.todos.filter((val, index) => {
+      return item !== val;
+    });
+
+    this.setState({
+      todos: updatedTodos
+    });
+  }
+
+
+  render () {
+    
+    var todos = this.state.todos;
+    var todo = todos.map((item, i) => {
+    return (
+      <TodoItem item={item} key={i} onDelete={this.onDelete} />
+      );
+    }) 
+
+    return (
+      <div className="todo-list">
+        <p>The busiest people have the most leisure time...</p>
+        <ul>
+          {todo}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Todo />, document.getElementById('todo-wrapper'));
